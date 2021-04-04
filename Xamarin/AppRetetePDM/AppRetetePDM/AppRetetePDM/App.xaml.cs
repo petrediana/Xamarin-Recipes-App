d57f9bb36d61ac263c7e155acdcd32b2e1aecb57
Diana Petre <petre.diana98@yahoo.ro>
2021-04-04 10:59:35 +0300

    using new NavigationPage

--- Xamarin/AppRetetePDM/AppRetetePDM/AppRetetePDM/App.xaml.cs
+++ Xamarin/AppRetetePDM/AppRetetePDM/AppRetetePDM/App.xaml.cs
@@ -10,7 +10,7 @@ namespace AppRetetePDM
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
